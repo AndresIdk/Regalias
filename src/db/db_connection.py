671e22605393043ba003a27e72992cacaee32e63
AndresIdk <jnavarrou@unicartagena.com>
2024-01-09 15:31:21 -0500

    Add updates

--- src/db/db_connection.py
+++ src/db/db_connection.py
@@ -2,21 +2,26 @@ import psycopg2
 from psycopg2 import DatabaseError
 import os
 from dotenv import load_dotenv
+import asyncio
 
 load_dotenv()
 
 class DBConnection:
-    def __init__(self) -> None:
-        try:
-            self.connection = psycopg2.connect(
-                host=os.getenv("DB_HOST"),
-                database=os.getenv("DB_NAME"),
-                user=os.getenv("DB_USER"),
-                password= os.getenv("DB_PASSWORD")
-            )
-            self.cursor = self.connection.cursor()
-        except DatabaseError as e:
-            raise e
+    async def __init__(self) -> None:
+        connected = False
+        while not connected:
+            try:
+                self.connection = await psycopg2.connect(
+                    host=os.getenv("DB_HOST"),
+                    database=os.getenv("DB_NAME"),
+                    user=os.getenv("DB_USER"),
+                    password=os.getenv("DB_PASSWORD")
+                )
+                self.cursor = await self.connection.cursor()
+                connected = True
+            except DatabaseError as e:
+                print(f"Error connecting to the database: {e}")
+                await asyncio.sleep(2)
     
     def execute_query(self, query, params=None):
         self.cursor.execute(query, params or None)
